ae5031770584cc47a724ad1e6cffff7b483d9ffc
Sabri0o <sabritrabelsi90@gmail.com>
2021-01-14 18:59:03 +0100

    fix sync db

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 ////////
 
 const db = require("./app/models");
-db.sequelize.sync({ force: true })
+db.sequelize.sync()
 .then(() => {
   console.log("Database connected !")
 })
